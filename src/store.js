b43fd74594940bc214bdd411c1d500b18fdc24d8
Youngestdev <laisibizness@gmail.com>
2020-05-24 00:36:15 +0100

    Delete todo

--- src/store.js
+++ src/store.js
@@ -1,10 +1,12 @@
 import { createStoreon } from "storeon";
+import { storeonDevtools } from 'storeon/devtools';
 
 let todos = store => {
     store.on('@init', () => ({ todos: [{id: 1, item:"Follow come todo lol."}, {id: 2, item: "Uhm. It works sha"}] }))
     store.on('addTodo', ({ todos }, todo) => ({ todos: todos.concat([{id: todos.length, item: todo}])}))
+    store.on('deleteTodo', ({ todos }, id) => ({ todos: todos.filter(todo => todo.id !== id) }))
 }
 
-const store = createStoreon([todos])
+const store = createStoreon([todos, process.env.NODE_ENV !== 'production' && storeonDevtools])
 
 export default store;
