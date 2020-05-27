4cb73ad7e508b68b4d8108eab0a30a552215397f
lilpolymath <ayofavour7@gmail.com>
2020-05-27 12:54:41 +0100

    feat: adjusted the styles.

--- src/Components/Todos.js
+++ src/Components/Todos.js
@@ -1,40 +1,44 @@
-import React from "react";
+import React from 'react';
 import { useStoreon } from 'storeon/react';
 
 //TODO: TURN THE APP INTO A FULL APP
 
-    const Todos = () => {
-    const {dispatch, notes } = useStoreon('notes')
-    const [value, setValue] = React.useState("")
+const Todos = () => {
+  const { dispatch, notes } = useStoreon('notes');
+  const [value, setValue] = React.useState('');
 
-    const deleteNote= id => {
-        dispatch('deleteNote', id)
-    }
+  const deleteNote = id => {
+    dispatch('deleteNote', id);
+  };
 
-    const submit = () => {
-        dispatch('addNote', value)
-        setValue("")
-    }
+  const submit = () => {
+    dispatch('addNote', value);
+    setValue('');
+  };
 
-    const handleInput = (e) => {
-        setValue(e.target.value)
-    }
-    return (
-        <>
-            <textarea onChange={handleInput} value={value}/><br/>
-            <button onClick={() => submit()}> Add A Note </button>
-            <hr />
-            <ul>
-                {notes.map(note => (
-                    <div className="box">
-                        <div className="container">
-                            <li key={note.id}>{note.item}</li><br />
-                            <button onClick={() => deleteNote(note.id)}>Delete note</button>
-                        </div>
-                    </div>
-                ))}
-            </ul>
-        </>
-    )
-}
+  const handleInput = e => {
+    setValue(e.target.value);
+  };
+  return (
+    <section>
+      <header>Quick Notes</header>
+
+      <div className='addNote'>
+        <textarea onChange={handleInput} value={value} />
+        <button onClick={() => submit()}> Add A Note </button>
+      </div>
+
+      <ul>
+        {notes.map(note => (
+          <li key={note.id}>
+            <div className='todo'>
+              <p>{note.item}</p>
+              <button onClick={() => deleteNote(note.id)}>Delete note</button>
+            </div>
+          </li>
+        ))}
+      </ul>
+    </section>
+  );
+};
 export default Todos;
--- src/index.js
+++ src/index.js
@@ -1,18 +1,18 @@
-import React from "react";
-import { render } from "react-dom";
-import { StoreContext } from 'storeon/react'
-import Todos from "./Components/Todos";
-import store from "../src/store";
+import React from 'react';
+import { render } from 'react-dom';
+import { StoreContext } from 'storeon/react';
+import Todos from './Components/Todos';
+import store from '../src/store';
 
 function App() {
-    return (
-        <>
-            <StoreContext.Provider value={store}>
-                <Todos />
-            </StoreContext.Provider>
-        </>
-    )
+  return (
+    <>
+      <StoreContext.Provider value={store}>
+        <Todos />
+      </StoreContext.Provider>
+    </>
+  );
 }
 
-const root = document.getElementById("root");
-render(<App />, root)
+const root = document.getElementById('root');
+render(<App />, root);
